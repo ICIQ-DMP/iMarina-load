bcc2de0a3dd120772504fe8e11fd42ebf542d7f2
Mario Piqué Martinez <mpique@iciq.es>
2025-09-17 12:37:37 +0200

    Create other test

---
+++ tests/test_2_main.py
@@ -0,0 +1,2 @@
+def test_researcher():
+    assert False
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,20 @@ def test_build_upload_excel():
 
 
 def test_upload_excel():
-    assert True
+    return True
+
+
+
+
 
 
 def test_build_translations():
     assert False
 
 
+def test_demo():
+    assert 1 + 1 == 2
+
+
+def test_researcher():
+    assert False
