33bbbd5da1f19d51e9f7beb38c709661c04017ea
Mario Piqué Martinez <mpique@iciq.es>
2025-09-17 08:46:25 +0200

    ADDED: Create a testing  by extension pytest for the archives main.py & arguments.py but only is a mini tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 ```
+=======
+>>>>>>> 9d5ea84 (ADDED: Create a testing  by extension pytest for the archives main.py & arguments.py but only is a mini tests)
 def test_build_upload_excel():
     assert False
 
@@ -9,4 +12,7 @@ def test_upload_excel():
 
 def test_build_translations():
     assert False
+<<<<<<< HEAD
 ```
+
+
