bb019bbeb9fc3a8b88633baa12bec9e64bfca923
Mario Piqué Martinez <mpique@iciq.es>
2025-09-17 11:36:24 +0200

    Try test_main by pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-```
-=======
->>>>>>> 9d5ea84 (ADDED: Create a testing  by extension pytest for the archives main.py & arguments.py but only is a mini tests)
+
 def test_build_upload_excel():
     assert False
 
 
 def test_upload_excel():
-    assert False
+    assert True
 
 
 def test_build_translations():
     assert False
-<<<<<<< HEAD
-```
 
 
